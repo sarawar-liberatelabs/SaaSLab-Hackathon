1cfe4b6eebe1ef2b3c4c9a0246d841e6dac435e3
Sarawar Hossain <sarawar@liberate-labs.com>
2025-05-02 11:41:55 +0600

    docker updated

--- AI-Backend/main.py
+++ AI-Backend/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from app.core.config import settings
 from app.api.v1.acquisition import AcquisitionRouter
 from app.api.v1.chat import ChatRouter
 from app.api.v1.user_feedback_research_agent import UserFeedbackResearchAgentRouter
-
-
+from app.middleware.middleware import add_logger_middleware
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
@@ -12,13 +12,12 @@ app = FastAPI(
     description="SaaSLab",
 )
 
-
+add_logger_middleware(app)
 
 app.include_router(ChatRouter, tags=["Chat"])
 app.include_router(AcquisitionRouter, tags=["Acquisition"])
 app.include_router(UserFeedbackResearchAgentRouter, tags=["User Feedback Research Agent"])
 
-@app.get("/", tags=["Root"])
-async def read_root():
-    return {"message": f"Welcome to the {settings.PROJECT_NAME}!"}
-
+@app.get("/", include_in_schema=False)
+def redirect_to_docs():
+    return RedirectResponse(url="/docs")
